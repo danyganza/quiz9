cf9a94f82c7189861d8c9eb4a0dbeb0c72cc1bfa
dannyganza <kingdanny@Dannys-Air.hitronhub.home>
2023-03-18 15:20:24 -0600

    added value

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let points =700;
+
+if (points>=1000){
+    console.log(`good job bud`);
+}else if(points<=500){
+    console.log(`great job, keep collecting`);
+} else{
+    console.log(`keep working and praying`)
+}
